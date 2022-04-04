8ea9fb0d554fda68535a1089524b0f1beeddded1
Alismos <daramirezs@eafit.edu.co>
2022-04-03 21:09:07 -0500

    update responsive navbar

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+/* Menu show and Hidden */
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+/* Menu show */
+
+// Validate if constant exists
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
+}
+
+/* Menu Hidden */
+
+// Validate if constant exists
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
+
+/* ==================== REMOVE MENU MOBILE ==================== */
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction () {
+  console.log('in')
+  const navMenu = document.getElementById('nav-menu')
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
