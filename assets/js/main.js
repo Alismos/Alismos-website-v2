d82459a8c7058a980c0601bceb8671adaef7c423
Alismos <daramirezs@eafit.edu.co>
2022-05-01 15:42:18 -0500

    add contact me functionality and scroll function

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,3 +94,37 @@ modalCloses.forEach((modalClose) => {
     })
   })
 })
+
+/* ==================== PORTFOLIO SWIPER ==================== */
+const swiper = new Swiper('.swiper-container', {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+})
+
+/* ==================== PORTFOLIO SWIPER ==================== */
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive () {
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50
+    sectionId = current.getAttribute('id')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    } else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
